ee0ae3bc1cc556df7485cdc50459d61fde03641e
KIRAIJI <yusi.jansdale@auf.edu.ph>
2026-09-24 17:06:45 +0800

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './context/CartContext.jsx';
+import App from './App.jsx';
+import './styles.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
